4043cb0f352399ce7cf0bc295677e43dff2adc9f
Fabiana Rado <luize.bazoli@gmail.com>
2023-12-21 11:05:02 -0300

    adicionou SDK firebase

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBadeywKFs4zBSbdUTjRYSpqtjrLunU_ps",
+    authDomain: "projeto67-f7b4e.firebaseapp.com",
+    databaseURL: "https://projeto67-f7b4e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "projeto67-f7b4e",
+    storageBucket: "projeto67-f7b4e.appspot.com",
+    messagingSenderId: "382503245067",
+    appId: "1:382503245067:web:f6fb364bc47fb3aac27e0e"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
